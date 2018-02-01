0ce8fc85eb33bb717ade376b924b918e6a3ca262
karel90 <karel.tasane@ttu.ee>
2018-02-01 17:59:54 +0200

    Smaller style changes

--- src/app/deposit-details/deposit-details.component.ts
+++ src/app/deposit-details/deposit-details.component.ts
@@ -35,17 +35,23 @@ export class DepositDetailsComponent implements OnInit {
     this.getDepositById(this.route.snapshot.paramMap.get('id'));
     this.mapService.drawDetailsViewMap();
     this.getDrillcoresByDepositId(this.route.snapshot.paramMap.get('id'));
+    
   }
 
   getDepositById(id: string): void {
-    this.depositService.searchDepositById(id).subscribe(deposit => { this.deposit = deposit['results'][0]; console.log(this.deposit); this.mapService.addPointWithName(this.deposit.name, this.deposit.longitude, this.deposit.latitude); });;
+    this.depositService.searchDepositById(id).subscribe(deposit => { this.deposit = deposit['results'][0]; 
+    console.log(this.deposit); 
+    this.mapService.addPointWithName(this.deposit.name, this.deposit.longitude, this.deposit.latitude); 
+  });
   }
 
   getDrillcoresByDepositId(id: string): void {
     var ID=id.toString();
     if (this.sites == undefined) {
-      this.siteService.searchSitesByDepositId(ID).subscribe(sites => { this.sites = sites['results']; console.log(this.sites) });
+      this.siteService.searchSitesByDepositId(ID).subscribe(sites => { this.sites = sites['results']; console.log(this.sites)
+    });
     }
   }
+
 
 }
